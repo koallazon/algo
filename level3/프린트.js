f1e1df6f6fbbe5e4c66989b13742604fd9ef846f
ltg <ltg@bbangyanetworks.com>
2022-03-30 21:25:24 +0900

    라인 추가 정리

--- level3/프린트.js
+++ level3/프린트.js
@@ -35,16 +35,16 @@ function solution(priorities, location) {
     const wait = [...docu]
     const print = []    
     function printDocu(arr) {
-		for (let i = 0; i < arr.length; i++) {
-          if (arr.some((v, idx) => priorities[docu.indexOf(v)] > priorities[docu.indexOf(arr[i])])) {
-          	const firstItem = wait.shift();
-            wait.push(firstItem);
-			break;
-          } else {
-            print.push(arr[i]);
-            wait.splice(i, 1);
-            break;
-          }
+        for (let i = 0; i < arr.length; i++) {
+            if (arr.some((v, idx) => priorities[docu.indexOf(v)] > priorities[docu.indexOf(arr[i])])) {
+                const firstItem = wait.shift();
+                wait.push(firstItem);
+                break;
+            } else {
+                print.push(arr[i]);
+                wait.splice(i, 1);
+                break;
+            }
         }
         return wait.length > 0 ? printDocu(wait) : false
     }
